11b7cd883884bcf0702bf741cf774c937f397e10
DunaevDS <dmitryesfo@yandex.ru>
2023-06-13 18:18:35 +0500

    fix:errors

--- src/managers/historymanagers/InMemoryHistoryManager.java
+++ src/managers/historymanagers/InMemoryHistoryManager.java
@@ -12,30 +12,24 @@ public class InMemoryHistoryManager implements HistoryManager {
     private final Map<Integer, Node<Task>> history;
 
     public InMemoryHistoryManager() {
-
         historyManager = new CustomLinkedList();
         history = new HashMap<>();
     }
 
     @Override
     public void add(Task task) {
-
         Node<Task> node = historyManager.linkLast(task);
-
         historyManager.removeNode(history.get(task.getTaskId()));
-
         history.put(task.getTaskId(), node);
     }
 
     @Override
     public void remove(int id) {
-
         historyManager.removeNode(history.remove(id));
     }
 
     @Override
     public List<Task> getHistory() {
-
         return historyManager.getTasks();
     }
 
--- src/managers/taskmanagers/HttpTaskManager.java
+++ src/managers/taskmanagers/HttpTaskManager.java
@@ -12,6 +12,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public class HttpTaskManager extends FileBackedTasksManager {
 
@@ -43,9 +44,9 @@ public class HttpTaskManager extends FileBackedTasksManager {
 
     public void load() {
         String jsonPrioritizedTasks = client.load("tasks");
-        Type prioritizedTaskType = new TypeToken<Collection<Task>>() {}.getType();
-        Collection<Task> priorityTasks = gson.fromJson(jsonPrioritizedTasks, prioritizedTaskType);
-        getPrioritizedTasks().addAll(priorityTasks);
+        Type prioritizedTaskType = new TypeToken<List<Task>>() {}.getType();
+        List<Task> priorityTasks = gson.fromJson(jsonPrioritizedTasks, prioritizedTaskType);
+        prioritizedTasks.addAll(priorityTasks);
 
         String gsonHistory = client.load("tasks/history");
         Type historyType = new TypeToken<List<Task>>() {}.getType();
@@ -53,33 +54,18 @@ public class HttpTaskManager extends FileBackedTasksManager {
         getHistory().addAll(history);
 
         String jsonTasks = client.load("tasks/task");
-        Type taskType = new TypeToken<Collection<Task>>() {}.getType();
-        Collection<Task> taskList = gson.fromJson(jsonTasks, taskType);
-        Collection<Task> tasks = getAllTasks();
-        try {
-            tasks.addAll(taskList);
-        } catch (NullPointerException e) {
-            tasks = new ArrayList<Task>(taskList);
-        }
+        Type taskType = new TypeToken<Map<Integer,Task>>() {}.getType();
+        Map<Integer,Task> taskList = gson.fromJson(jsonTasks, taskType);
+        tasks.putAll(taskList);
 
         String jsonEpics = client.load("tasks/epic");
-        Type epicType = new TypeToken<Collection<Epic>>() {}.getType();
-        Collection<Epic> epicList = gson.fromJson(jsonEpics, epicType);
-        Collection<Epic> epics = getAllEpics();
-        try {
-            epics.addAll(epicList);
-        } catch (NullPointerException e) {
-            epics = new ArrayList<Epic>(epicList);
-        }
+        Type epicType = new TypeToken<Map<Integer, Epic>>() {}.getType();
+        Map<Integer,Epic> epicList = gson.fromJson(jsonEpics, epicType);
+        epics.putAll(epicList);
 
         String jsonSubtasks = client.load("tasks/subtask");
-        Type subtaskType = new TypeToken<Collection<Subtask>>() {}.getType();
-        Collection<Subtask> subtasksList = gson.fromJson(jsonSubtasks, subtaskType);
-        Collection<Subtask> subtasks = getAllSubtasks();
-        try {
-            subtasks.addAll(subtasksList);
-        } catch (NullPointerException e) {
-            subtasks = new ArrayList<Subtask>(subtasksList);
-        }
+        Type subtaskType = new TypeToken<Map<Integer, Subtask>>() {}.getType();
+        Map<Integer, Subtask> subtasksList = gson.fromJson(jsonSubtasks, subtaskType);
+        subtasks.putAll(subtasksList);
     }
 }
